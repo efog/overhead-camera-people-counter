29b650ecb4d6707c4aeabaf357f3df2b9988967a
Etienne Brouillard <etienne.brouillard@outlook.com>
2018-02-15 12:54:29 -0500

    readding X11 out option

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include "help.cpp"
 #include "modules/counter.hpp"
 #include "modules/console.hpp"
+#include "modules/view.hpp"
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) return help();
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return help();
 
-    VideoCapturePeopleCounter* counter = new VideoCapturePeopleCounter(argv[1]);
-    counter->delegate = new ConsoleController(counter);
+    VideoCapturePeopleCounter *counter = new VideoCapturePeopleCounter(argv[1]);
+    if (argc == 3 && argv[2] == "v")
+    {
+        counter->delegate = new ConsoleController(counter);
+    }
+    else
+    {
+        counter->delegate = new WindowController(counter);
+    }
     counter->setRefLineY(120);
     counter->start();
 }
